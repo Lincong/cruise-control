417b08473be635ac0658af52610129b15855e1b3
Lincong Li <lcli@linkedin.com>
2020-11-16 14:55:33 -0800

    Calcualte balance lower bound for resource distribution lower bound with low utilization threshold

--- cruise-control-core/src/main/java/com/linkedin/cruisecontrol/common/utils/Utils.java
+++ cruise-control-core/src/main/java/com/linkedin/cruisecontrol/common/utils/Utils.java
@@ -7,6 +7,7 @@ package com.linkedin.cruisecontrol.common.utils;
 import com.linkedin.cruisecontrol.exception.CruiseControlException;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Objects;
 
 
 public class Utils {
